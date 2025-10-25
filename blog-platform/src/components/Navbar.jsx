a28fb2d507f6ba4eec5154b131880b43abecf31f
Oswaldo Cabrera <124094692+tacomcfancy@users.noreply.github.com>
2025-10-25 15:56:47 -0700

    files for this project

---
+++ blog-platform/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <nav style={{ padding: "10px", backgroundColor: "#333", color: "#fff"}}>
+            <h1>Hello World Blog Version</h1>
+        </nav>
+    );
+}
---
+++ blog-platform/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <main style={{ padding: "20px " }}>
+            <h2>Welcome to The Blog</h2>
+            <p>This is where things will be posted.</p>
+        </main>
+    );
+}
