14f837d691b945c78589ce7952fb4a2905735173
Oswaldo Cabrera <124094692+tacomcfancy@users.noreply.github.com>
2025-10-31 18:21:46 -0700

    worked more on the posting section of the code

--- blog-platform/src/pages/Home.jsx
+++ blog-platform/src/pages/Home.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { posts } from "../data/posts";
 
 export default function Home() {
     return (
         <main style={{ padding: "20px " }}>
             <h2>Welcome to The Blog</h2>
-            <p>This is where things will be posted.</p>
+            <ul style={{ listStyle: "none", padding: 0 }}>
+                {posts.map((post) => (
+                    <li key={post.id} style={{ marginBottom: "15px" }}>
+                        <Link
+                            to={`/post/${post.id}`}
+                            style={{
+                                color: "#007bff",
+                                textDecoration: "none",
+                                fontSize: "18px",
+                            }}
+                            >
+                            {post.title}
+                        </Link>
+                        <p style={{ margin: "5px 0" }}>
+                            {post.content.substring(0, 60)}...
+                        </p>
+                        <hr />
+                    </li>
+                ))}
+            </ul>
         </main>
     );
 }clearInterval
