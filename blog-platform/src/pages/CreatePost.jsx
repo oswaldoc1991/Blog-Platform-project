3148398bcc7370b476187e7435d8b070c49a9e33
Oswaldo Cabrera <124094692+tacomcfancy@users.noreply.github.com>
2025-11-13 15:11:13 -0800

    going to make sure that the CreatePost.jsx works

--- blog-platform/src/pages/CreatePost.jsx
+++ blog-platform/src/pages/CreatePost.jsx
@@ -1,10 +1,86 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { posts } from "../data/posts";
 
 export default function CreatePost() {
-    return (
-        <main className="p-6">
-            <h2>Create a New post</h2>
-            <p>This pagev will allow users to create a new blog post</p>
-        </main>
-    );
+   const [title, setTitle] = useState("");
+   const [content, setContent] = useState("");
+   const navigate = useNavigate();
+
+   const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        // this will create a new post 
+        const newPost = {
+            id: posts.Length + 1,
+            title,
+            content,
+        };
+
+        // this will add the new post to the posts array
+        posts.push(newPost);
+
+        // this will help go back to the home page
+        navigate("/");
+   };
+
+   return (
+    <main style={{ padding: "20px" }}>
+        <h1>Create a New Post</h1>
+        <form onSubmit={handleSubmit}>
+            <div style={{ marginBottom: "15px" }}>
+                {/* code for title starts */}
+                <label>Title: </label>
+                <br />
+                <input 
+                    type="text"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    required
+                    style={{
+                        width: "100%",
+                        padding: "10px",
+                        fontSize: "16px",
+                        marginTop: "5px",
+                    }}
+                />
+            </div>
+            {/* code for title ends */}
+            
+            {/* code for content starts */}
+            <div style={{ marginBottom: "15px "}}>
+                <lable>Content: </lable>
+                <br />
+                <textarea 
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                    required
+                    rows="11"
+                    style={{
+                        width: "100%",
+                        padding: "10px",
+                        fontSize: "16px",
+                        marginTop: "5px",
+                    }}
+                />
+            </div>
+            {/* code for content ends here */}
+
+            {/* code for button */}
+            <button 
+                type="submit"
+                style={{
+                    backgroundColor: "#007bff",
+                    color: "#fff",
+                    padding: "10px 20px",
+                    border: "none",
+                    cursor: "pointer",
+                }}
+            >
+                Submit Post
+            </button>
+            {/* code for button */}
+        </form>
+    </main>
+   )
 }
